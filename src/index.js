5c0952d1a4382127dd9c7146944cb22fcfe34754
Leonardo Scarcelli <leonardoscarcelli@hotmail.com>
2025-01-14 11:46:05 -0300

    penúltima atualização

--- src/index.js
+++ src/index.js
@@ -2,7 +2,12 @@ import React from 'react';
 import ReactDOM, { createRoot } from 'react-dom/client';
 import './styles/global-styles.css';
 import { Home } from './templates/Home';
+import { CounterContextProvider } from './contexts/CounterContext';
 
 const container = document.getElementById('root');
 const root = createRoot(container);
-root.render(<Home tab="home" />);
+root.render(
+  <CounterContextProvider>
+    <Home tab="home" />
+  </CounterContextProvider>,
+);
--- src/templates/Home/index.jsx
+++ src/templates/Home/index.jsx
@@ -1,7 +1,18 @@
+import { useEffect, useState } from 'react';
+import { Button } from '../../components/Button';
+import { Heading } from '../../components/Heading';
+import { useCounterContext } from '../../contexts/CounterContext';
+
 export const Home = () => {
+  const [state, actions] = useCounterContext();
+
   return (
     <div>
-      <h1>Oi</h1>
+      <Heading />
+
+      <div>
+        <Button onButtonClick={actions.increase}>Increase</Button>
+      </div>
     </div>
   );
 };
